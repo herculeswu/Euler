c091ec8b9b2a358f763535cc127faacaf94171e7
Teng <HerculesWu@gmail.com>
2014-05-09 16:01:09 -0400

    The first commit

---
+++ Euler/src/ca/uwaterloo/comsec/euler/solutions/P469/Node.java
@@ -0,0 +1,11 @@
+package ca.uwaterloo.comsec.euler.solutions.P469;
+
+public class Node {
+
+	private Node m2;
+	private Node m3;
+	private int value;
+	private long subTotal;
+	
+
+}
---
+++ Euler/src/ca/uwaterloo/comsec/euler/solutions/P469/P469EmptyChairs.java
@@ -0,0 +1,17 @@
+package ca.uwaterloo.comsec.euler.solutions.P469;
+
+public class P469EmptyChairs {
+	
+	public static void solve() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
